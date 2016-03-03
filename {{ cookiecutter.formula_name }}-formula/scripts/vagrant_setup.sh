73d43deadb5ad07c07072f78bce551066e1ef265
Tobias Macey <tmacey@mit.edu>
2016-03-03 09:49:53 -0500

    Updated setup scripts
    Updated the setup and test scripts to account for differences in the
    default sync directory for vagrant boxes.

--- {{ cookiecutter.formula_name }}-formula/scripts/vagrant_setup.sh
+++ {{ cookiecutter.formula_name }}-formula/scripts/vagrant_setup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+if [ "$(ls /vagrant)" ]
+then
+    SRCDIR=/vagrant
+else
+    SRCDIR=/home/vagrant/sync
+fi
 sudo mkdir -p /srv/salt
 sudo mkdir -p /srv/pillar
 sudo mkdir -p /srv/formulas
-sudo cp /vagrant/pillar.example /srv/pillar/pillar.sls
-sudo cp -r /vagrant/{{ cookiecutter.formula_name }} /srv/salt
+sudo cp $SRCDIR/pillar.example /srv/pillar/pillar.sls
+sudo cp -r $SRCDIR/{{ cookiecutter.formula_name }} /srv/salt
 echo "\
 base:
   '*':
