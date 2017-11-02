792e3fa38f7dddaced3833fd39d15be46218df6c
Dave Neeley <dneeley@spillman.com>
2017-11-01 21:47:27 -0600

    Add Salt Package Manager Support

--- {{ cookiecutter.formula_name }}-formula/scripts/vagrant_setup.sh
+++ {{ cookiecutter.formula_name }}-formula/scripts/vagrant_setup.sh
@@ -9,10 +9,14 @@ fi
 sudo mkdir -p /srv/salt
 sudo mkdir -p /srv/pillar
 sudo mkdir -p /srv/formulas
+{% if cookiecutter.deploy_salt_packages == "True" %}
+sudo mkdir -p /var/cache/salt/master
+{% else %}
 sudo cp $SRCDIR/pillar.example /srv/pillar/pillar.sls
 sudo cp -r $SRCDIR/{{ cookiecutter.formula_name }} /srv/salt
 echo "\
 base:
   '*':
     - pillar" | sudo tee /srv/pillar/top.sls
+{% endif %}
 sudo cp $SRCDIR/salt-top.example /srv/salt/top.sls
