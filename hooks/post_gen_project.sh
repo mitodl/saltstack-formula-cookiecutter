658c542f1f91815dc4ef4ba77c0635d0235497cd
Stoo Johnston <scj@stooj.org>
2018-03-02 17:42:26 +0000

    Fix typo of initial commit message

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -4,4 +4,4 @@ git init
 git add -A .
 git config user.name "{{ cookiecutter.full_name }}"
 git config user.email "{{ cookiecutter.email_address }}"
-git commit -m 'Initial commit after generating formula from https://github.com/mitodl/saltstack-formula-cookiectuter'
+git commit -m 'Initial commit after generating formula from https://github.com/mitodl/saltstack-formula-cookiecutter'
